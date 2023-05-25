8931f3e3dd2fc47c0fce64142650867a337de84d
evgeniac10 <rlagustjd2684@gmail.com>
2023-05-25 14:14:09 +0900

    계단오르기 미완성
    .

---
+++ 현성/DP/백준_2579번.java
@@ -0,0 +1,32 @@
+package 백준;
+
+import java.util.*;
+
+public class 백준_2579번 {
+	
+	static int stair(int num, int arr[]) {
+		int result=0;
+		
+		
+		
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int arr[] = new int[num];
+//		Integer dp[] = new Integer [num];
+		
+		for(int i=0 ; i<num; i++) {
+			arr[i] = sc.nextInt();
+		}
+//		for(int x :arr) {
+//			System.out.println(x);
+//		}
+		
+	}
+
+}
