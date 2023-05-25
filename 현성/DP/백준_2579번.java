5417eda3a32655f7be427d033f28e16f97f8eadc
evgeniac10 <rlagustjd2684@gmail.com>
2023-05-25 15:56:05 +0900

    계단오르기 완성본
    .

--- 현성/DP/백준_2579번.java
+++ 현성/DP/백준_2579번.java
@@ -1,32 +1,25 @@
 package 백준;
 
-import java.util.*;
-
-public class 백준_2579번 {
-	
-	static int stair(int num, int arr[]) {
-		int result=0;
-		
-		
-		
-		
-		return result;
-	}
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int num = sc.nextInt();
-		int arr[] = new int[num];
-//		Integer dp[] = new Integer [num];
-		
-		for(int i=0 ; i<num; i++) {
-			arr[i] = sc.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class 백준_2579번{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		int f[] = new int[T+1];
+		int cache[] = new int[T+1];
+		for(int i = 1; i<=T; i++){
+			f[i] = Integer.parseInt(br.readLine());
 		}
-//		for(int x :arr) {
-//			System.out.println(x);
-//		}
-		
+		cache[1] = f[1];
+		cache[2] = f[2]+f[1];
+		cache[3]= Math.max(f[1]+f[3], f[2]+f[3]);
+		for(int i=4; i<=T; i++){
+			cache[i] = Math.max(cache[i-3]+f[i]+f[i-1], cache[i-2]+f[i]);
+		}
+		System.out.println(cache[T]);
 	}
-
+	
 }
