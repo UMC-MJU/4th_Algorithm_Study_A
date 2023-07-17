2e4a793002356a41c1c33a2d72ac79a200cb83b0
Gyehwan Baek <b99hwan@gmail.com>
2023-07-17 17:28:58 +0900

    [FEAT]: DP 문제 풀이

--- 계환/DP/01타일(1904).py
+++ 계환/DP/01타일(1904).py
@@ -1,17 +1,26 @@
 # 01타일 https://www.acmicpc.net/problem/1904
 
-import math  # 팩토리얼 메서드를 사용하기 위함
 import sys
 
 input = sys.stdin.readline
 
 N = int(input())
 
-answer = 0
-div = N // 2
+# dp = [0] * (N+1)   // N+1 사이즈로 선언하면 IndexError
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
 
-for i in range(div+1):
-    answer += math.comb(N-i, i)
+for i in range(3, N + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
 
-answer %= 15746
-print(int(answer))
+print(dp[N])
+
+# 시간초과 풀이 (조합)
+# answer = 0
+# div = N // 2
+#
+# for i in range(div+1):
+#     answer += math.comb(N-i, i)
+#
+# answer %= 15746
