f976278a0469831da3f398e0aef5725b195e2ffd
Gyehwan Baek <b99hwan@gmail.com>
2023-09-20 14:23:26 +0900

    Update 보석상자.java

--- 계환/BOJ/보석상자.java
+++ 계환/BOJ/보석상자.java
@@ -18,24 +18,28 @@ public class 보석상자 {
         M = Integer.parseInt(st.nextToken());
 
         int color[] = new int[M]; // 색상 배열
+        // sum -> 한 명 줄 때마다 +1
         int start = 1, end = 0, mid, sum = 0;
 
         for (int i = 0; i < M; i++) {
             color[i] = Integer.parseInt(reader.readLine());
             end = Math.max(end, color[i]);  // 오른쪽 끝값 구하기
         }
-        // 이분탐색
+
+        // 이분 탐색 시작
         while (start <= end) {
             mid = (start + end) / 2;
+            sum = 0;
+
             for (int i = 0; i < M; i++) {
-                if (color[i] > mid) {
-                    sum += color[i] / mid + 1;
-                }else{
+                if (color[i] % mid == 0) {
                     sum += color[i] / mid;
+                }else{
+                    sum += color[i] / mid + 1;
                 }
             }
 
-            if (sum < N) {
+            if (sum <= N) {
                 end = mid - 1;
                 answer = mid;
             }else{
