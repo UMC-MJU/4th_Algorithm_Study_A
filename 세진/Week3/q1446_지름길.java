2de47c8ee98658979deb61ebcdbb715feb44a835
Park Sejin <jinpark99@naver.com>
2023-09-27 14:37:09 +0900

    2주차 박세진

--- 세진/Week3/q1446_지름길.java
+++ 세진/Week3/q1446_지름길.java
@@ -1,12 +1,12 @@
-package Week3;
-
 import java.util.*;
 
 public class q1446_지름길 {
+
     static final int INF = Integer.MAX_VALUE;
-    static ArrayList<Edge>[] graph;
-    static int[] dist;
+    static ArrayList<Edge>[] graph; // 그래프
+    static int[] dist; // 다익스트라 거리 배열
     static int N, D;
+
     static class Edge implements Comparable<Edge> {
         private final int w; // 비용
         private final int v; // 정점
@@ -16,25 +16,29 @@ public class q1446_지름길 {
         }
         @Override
         public int compareTo(Edge edge) {
-            return this.w - edge.w;
+            return edge.w - this.w;
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        graph = new ArrayList[D];
-        for (int i = 0; i < D; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        dist = new int[D];
-        Arrays.fill(dist, INF);
         N = sc.nextInt();
         D = sc.nextInt();
+        graph = new ArrayList[D + 1];
+        for (int i = 0; i <= D; i++) {
+            graph[i] = new ArrayList<>();
+            if(i < D) graph[i].add(new Edge(1, i + 1)); // 고속도로의 모든 부분을 점으로 바꿔 다음 그래프 추가
+        }
+        dist = new int[D + 1];
+        Arrays.fill(dist, INF);
         for (int i = 0; i < N; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
             int w = sc.nextInt();
-            graph[u].add(new Edge(w, v));
+            if(v <= D) graph[u].add(new Edge(w, v));
         }
+
+        // 밑은 다익스트라 알고리즘
         PriorityQueue<Edge> pq = new PriorityQueue<>(Edge::compareTo);
         dist[0] = 0;
         pq.add(new Edge(dist[0], 0));
@@ -47,6 +51,6 @@ public class q1446_지름길 {
                 pq.add(new Edge(dist[nxt.v], nxt.v));
             }
         }
-        System.out.println(dist[D - 1]);
+        System.out.println(dist[D]);
     }
 }
