477f7bf3e6401253a76d66f2493ffcacdcf368f5
devholic22 <hyunjoon.tech@gmail.com>
2023-10-15 16:00:50 +0900

    solve: 여러 기준으로 정렬하기 (sql)

---
+++ 현준/23-2/week6/sql/여러_기준으로_정렬하기.sql
@@ -0,0 +1,4 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, DATETIME FROM ANIMAL_INS ORDER BY NAME, DATETIME DESC;
+-- ORDER BY 때 여러 개 정렬 가능 (첫 번째 column으로 정렬한 뒤 이후 column으로 정렬함)
+-- 기본 ASC (오름차순), 특별하게 할 경우 뒤에 DESC (내림차순) 붙임
