2a5d2f151eb18c9d0936af22b82158646b04ee87
hyunseong <rlagustjd2684@gmail.com>
2023-11-21 22:23:32 +0900

    후위 표기식 (미완성)

---
+++ 현성/Implement/Postfix.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Postfix {
+
+    public static void main(String[] args) {
+
+        String name = new String("김현성");
+        boolean isName = false;
+
+        if(name.equals( "김현성")){
+            isName = true;
+        }
+        System.out.println("isName = " + isName);
+
+    }
+}
