364ce50e3f38945effe532e6bee3c1f474ed623f
choeun7 <95676587+choeun7@users.noreply.github.com>
2023-11-21 20:18:06 +0900

    Revert "9주차 초은"

--- 초은/SQL/진료과별 총 예약 횟수 출력하기.sql
+++
@@ -1,8 +0,0 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/132202
-SELECT
-MCDP_CD AS "진료과코드",
-COUNT(MCDP_CD) AS "5월예약건수"
-FROM APPOINTMENT
-WHERE TO_CHAR(APNT_YMD, 'YYYYMM') = '202205'
-GROUP BY MCDP_CD
-ORDER BY COUNT(MCDP_CD), MCDP_CD
--- 초은/SQL/최댓값 구하기.sql
+++
@@ -1,6 +0,0 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/59415
-SELECT DATETIME AS 시간
-FROM (SELECT *
-     FROM ANIMAL_INS
-     ORDER BY DATETIME DESC)
-WHERE ROWNUM = 1
