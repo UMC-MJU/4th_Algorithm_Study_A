d093432d94a42f2f80ad0085cf9126df223b3253
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-03-31 09:17:30 +0900

    백준 2579번 계단오르기

--- 현성/DP/백준_2579번.java
+++ 현성/DP/백준_2579번.java
@@ -7,19 +7,30 @@ import java.io.InputStreamReader;
 public class 백준_2579번{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		int f[] = new int[T+1];
-		int cache[] = new int[T+1];
-		for(int i = 1; i<=T; i++){
-			f[i] = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());;
+		
+		arr = new Integer[N+1];
+		dp = new Integer[N+1];
+		
+		for(int i = 1 ; i <= N ; i++) {
+				arr[i]=Integer.parseInt(br.readLine());
 		}
-		cache[1] = f[1];
-		cache[2] = f[2]+f[1];
-		cache[3]= Math.max(f[1]+f[3], f[2]+f[3]);
-		for(int i=4; i<=T; i++){
-			cache[i] = Math.max(cache[i-3]+f[i]+f[i-1], cache[i-2]+f[i]);
+		
+		dp[0] = 0;
+		dp[1] = arr[1];
+		
+		if(N>1)
+		dp[2] = arr[2]+arr[1];
+		
+		System.out.println(max(N));
+		
+}
+	public static int max(int num) {
+		
+		if(dp[num]==null) {
+			dp[num] = Math.max(max(num-2), max(num-3)+arr[num-1])+arr[num];
 		}
-		System.out.println(cache[T]);
+		return dp[num];
 	}
 	
 }
