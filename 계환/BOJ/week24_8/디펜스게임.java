f528f7cfac9d4985d45361961de398f9aa981bf3
Gyehwan Baek <b99hwan@gmail.com>
2024-05-12 19:29:17 +0900

    Add files via upload

---
+++ 계환/BOJ/week24_8/디펜스게임.java
@@ -0,0 +1,23 @@
+package BOJ.week24_8;
+
+import java.util.*;
+public class 디펜스게임 {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = enemy.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i < enemy.length; i++){
+            n -= enemy[i];
+            pq.add(enemy[i]);
+            if(n < 0){
+                if(k > 0 && !pq.isEmpty()){
+                    n += pq.poll();
+                    k--;
+                } else {
+                    answer = i;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
